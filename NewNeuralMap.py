af19712851e01ac59e170fec920e0c4dd822be2b
Krzysztof_Tomkow <218514@student.pwr.edu.pl>
2018-11-30 15:47:53 +0100

    Images loaded and processed by many threads at once

--- NewNeuralMap.py
+++ NewNeuralMap.py
@@ -166,7 +166,7 @@ class NewNeuralMap:
     def print(self):
         array = self.convert_to_print()
         img = Image.fromarray(array, 'RGB')
-        img = img.resize((600,600))
+        img = img.resize((300,300))
         img.show()
 
     def print_weights(self):
